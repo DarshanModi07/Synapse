a5b47c7a98e7d46fcadd347766a7750f6c3b46d4
DarshanModi07 <darshanmodi016@gmail.com>
2026-06-08 09:07:08 +0000

    created getAllDept

--- backend/src/Controller/teamController.js
+++ backend/src/Controller/teamController.js
@@ -80,3 +80,73 @@ export const createTeam = async (req,res) => {
         })
     }
 }
+
+export const getAllTeams = async (req,res) => {
+    try{
+        const { departmentId } = req.params
+
+        if(!departmentId){
+            return res.status(400).json({
+                message:"credential needed"
+            })
+        }
+
+        const userId = req.user.userId
+
+        const checkDept = await prisma.department.findUnique({
+            where:{
+                id:departmentId
+            }
+        })
+
+        if(!checkDept){
+            return res.status(404).json({
+                message:"Department Not Found"
+            })
+        }
+
+        const checkUser = await prisma.workspaceMember.findUnique({
+            where:{
+                workspaceId_userId:{
+                    workspaceId:checkDept.workspaceId,
+                    userId
+                }
+            }
+        })
+
+        if(!checkUser){
+            return res.status(403).json({
+                message:"You are not a member of this workspace"
+            })
+        }
+
+        const getAllDept = await prisma.team.findMany({
+            where:{
+                departmentId,
+                is_deleted:false
+            },
+            orderBy:{
+                createdAt:"desc"
+            }
+        })
+
+        if(getAllDept.length == 0){
+            return res.status(200).json({
+                message:"No Teams found",
+                data:getAllDept
+            })
+        }
+
+        return res.status(200).json({
+            message:"All Department fetched",
+            data:getAllDept
+        })
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"Internal Server error during get all teams"
+        })
+    }
+}
--- backend/src/routes/teamRoute.js
+++ backend/src/routes/teamRoute.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { authMiddleware } from "../middlewares/authMiddleware.js"
-import { createTeam } from '../Controller/teamController.js';
+import { createTeam, getAllTeams } from '../Controller/teamController.js';
 
 const router = Router();
 
 router.post("/createTeam",authMiddleware,createTeam)
+router.get("/:departmentId/teams",authMiddleware,getAllTeams)
 
 export default router;
