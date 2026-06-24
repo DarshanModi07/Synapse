ddf1ba5bef4f07c35d8f7ae7050b68e994a26102
DarshanModi07 <darshanmodi016@gmail.com>
2026-06-24 18:19:19 +0000

    update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "../pages/landingPage.jsx";
+import RegisterPage from "../pages/RegisterPage.jsx"
+import LoginPage from "../pages/LoginPage.jsx"
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
