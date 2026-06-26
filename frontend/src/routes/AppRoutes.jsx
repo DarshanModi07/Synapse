ea550d06719bee4d41fd2f9794a17890d82c5551
DarshanModi07 <darshanmodi016@gmail.com>
2026-06-26 06:57:31 +0000

    update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,15 +1,57 @@
 import { Routes, Route } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext.jsx";
 
-import LandingPage from "../pages/landingPage.jsx";
-import RegisterPage from "../pages/RegisterPage.jsx"
-import LoginPage from "../pages/LoginPage.jsx"
+import LandingPage from "@/pages/landingPage.jsx";
+import  WorkspacePage  from "@/pages/WorkspacePage.jsx";
+import  RegisterPage  from "@/pages/RegisterPage.jsx";
+import  LoginPage  from "@/pages/LoginPage.jsx";
 
 const AppRoutes = () => {
+  const { profile, loading } = useAuth();
+
+  console.log("loading : ",loading)
+  console.log("profile : ",profile)
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (profile) {
+    return (
+      <Routes>
+        <Route
+          path="*"
+          element={<WorkspacePage />}
+        />
+      </Routes>
+    );
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={<LandingPage />}
+      />
+
+      <Route
+        path="/register"
+        element={<RegisterPage />}
+      />
+
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+
+      <Route
+        path="*"
+        element={<LandingPage />}
+      />
     </Routes>
   );
 };
